6e7778c3c0bde2ea8d3982b128db32057308031b
Valeriy Kovalenko <vecalion@gmail.com>
2019-06-04 19:35:45 +0300

    initial commit

---
+++ FloatingLabels/Controls/FloatingLabelInput.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace FloatingLabels.Controls
+{
+    public partial class FloatingLabelInput : ContentView
+    {
+        int _placeholderFontSize = 18;
+        int _titleFontSize = 14;
+        int _topMargin = -32;
+
+        public event EventHandler Completed;
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create("Text", typeof(string), typeof(string), string.Empty, BindingMode.TwoWay, null, HandleBindingPropertyChangedDelegate);
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(string), string.Empty, BindingMode.TwoWay, null);
+        public static readonly BindableProperty ReturnTypeProperty = BindableProperty.Create(nameof(ReturnType), typeof(ReturnType), typeof(FloatingLabelInput), ReturnType.Default);
+        public static readonly BindableProperty IsPasswordProperty = BindableProperty.Create("IsPassword", typeof(bool), typeof(FloatingLabelInput), default(bool));
+        public static readonly BindableProperty KeyboardProperty = BindableProperty.Create("Keyboard", typeof(Keyboard), typeof(FloatingLabelInput), Keyboard.Default, coerceValue: (o, v) => (Keyboard)v ?? Keyboard.Default);
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public ReturnType ReturnType
+        {
+            get => (ReturnType)GetValue(ReturnTypeProperty);
+            set => SetValue(ReturnTypeProperty, value);
+        }
+
+        public bool IsPassword
+        {
+            get { return (bool)GetValue(IsPasswordProperty); }
+            set { SetValue(IsPasswordProperty, value); }
+        }
+
+        public Keyboard Keyboard
+        {
+            get { return (Keyboard)GetValue(KeyboardProperty); }
+            set { SetValue(KeyboardProperty, value); }
+        }
+
+        static async void HandleBindingPropertyChangedDelegate(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = bindable as FloatingLabelInput;
+            if (!control.EntryField.IsFocused)
+            {
+                if (!string.IsNullOrEmpty((string)newValue))
+                {
+                    await control.MoveLabelToTop(false);
+                }
+                else
+                {
+                    await control.MoveLabelToMiddle(false);
+                }
+            }
+        }
+
+        public FloatingLabelInput()
+        {
+            InitializeComponent();
+            LabelTitle.TranslationX = 10;
+            LabelTitle.FontSize = _placeholderFontSize;
+        }
+
+        public new void Focus()
+        {
+            if (IsEnabled)
+            {
+                EntryField.Focus();
+            }
+        }
+
+        async void Handle_Focused(object sender, FocusEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                await MoveLabelToTop(true);
+            }
+        }
+
+        async Task MoveLabelToTop(bool animated)
+        {
+            if (animated)
+            {
+                var t1 = LabelTitle.TranslateTo(0, _topMargin, 100);
+                var t2 = SizeTo(_titleFontSize);
+                await Task.WhenAll(t1, t2);
+            }
+            else
+            {
+                LabelTitle.TranslationX = 0;
+                LabelTitle.TranslationY = -30;
+                LabelTitle.FontSize = 14;
+            }
+        }
+
+        async Task MoveLabelToMiddle(bool animated)
+        {
+            if (animated)
+            {
+                var t1 = LabelTitle.TranslateTo(10, 0, 100);
+                var t2 = SizeTo(_placeholderFontSize);
+                await Task.WhenAll(t1, t2);
+            }
+            else
+            {
+                LabelTitle.TranslationX = 10;
+                LabelTitle.TranslationY = 0;
+                LabelTitle.FontSize = _placeholderFontSize;
+            }
+        }
+
+        async void Handle_Unfocused(object sender, FocusEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                await MoveLabelToMiddle(true);
+            }
+        }
+
+        void Handle_Tapped(object sender, EventArgs e)
+        {
+            if (IsEnabled)
+            {
+                EntryField.Focus();
+            }
+        }
+
+        Task SizeTo(int fontSize)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            // setup information for animation
+            Action<double> callback = input => { LabelTitle.FontSize = input; };
+            double startingHeight = LabelTitle.FontSize;
+            double endingHeight = fontSize;
+            uint rate = 5;
+            uint length = 100;
+            Easing easing = Easing.Linear;
+
+            // now start animation with all the setup information
+            LabelTitle.Animate("invis", callback, startingHeight, endingHeight, rate, length, easing, (v, c) => taskCompletionSource.SetResult(c));
+
+            return taskCompletionSource.Task;
+        }
+
+        void Handle_Completed(object sender, EventArgs e)
+        {
+            Completed?.Invoke(this, e);
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == nameof(IsEnabled))
+            {
+                EntryField.IsEnabled = IsEnabled;
+            }
+        }
+    }
+}
